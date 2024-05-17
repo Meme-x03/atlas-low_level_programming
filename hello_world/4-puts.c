acc02dc846eb424894abc4809d76f126f91219cc
Meme-x03 <jmemelas.03@hotmail.com>
2024-05-17 14:50:44 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints "Programming is like building a multilingual
- *		  puzzle, followed by a newline.
- * Return: Always 0.
+ * main - A program that prints a line with puts fuction 
+ * Return : 0 (Success)
  */
-
 int main(void)
 {
-	puts("\"Programming is like a multilingual puzzle");
+	puts("\"Programing is like building a multilingual puzzle");
 
 	return (0);
 }
