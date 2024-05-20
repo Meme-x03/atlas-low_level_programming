db968c7ae330909254685c4a64217b10a05ef221
Meme-x03 <jmemelas.03@hotmail.com>
2024-05-20 02:34:32 +0000

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Numbers
+ * Return: 1 Always (Success)
+ */
+
+int main(void)
+
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	printf("%d", i);
+	printf("\n");
+
+	return (0);
+}
