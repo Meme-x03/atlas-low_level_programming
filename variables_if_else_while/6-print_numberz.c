ca20054066df7d7c3b3a54ba3e556bc6aa8086e1
Meme-x03 <jmemelas.03@hotmail.com>
2024-05-20 02:39:38 +0000

    6-print_numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/* 
+ * main - Numberz 
+ * Always Success
+ */
+
+int main(void)
+
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
+	putchar('\n');
+
+	return (0);
+}
