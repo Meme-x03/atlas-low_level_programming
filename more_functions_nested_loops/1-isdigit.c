245f93571e1b58e5d8edff64a2c819b3282f0543
Meme-x03 <jmemelas.03@hotmail.com>
2024-05-24 15:13:52 +0000

    1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * isdigit - Check is a number is a digit
+ * Return: Always 0 (Sucess)
+ */
+
+int _isdigit(int c);
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	return (0);
+}
+
