65532a5932f1d8e9a36ff0e4507e875ab38369fa
Meme-x03 <jmemelas.03@hotmail.com>
2024-07-07 23:44:03 +0000

    a

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -17,7 +17,7 @@ unsigned int binary_to_uint(const char *b)
 	for (a = 0; b[a] != '\0'; a++)
 	{
 		if (b[a] != '0' && b[a] != '1')
-			return(0);
+			return (0);
 	}
 	for (a = 0; b[a] != '\0'; a++)
 	{
