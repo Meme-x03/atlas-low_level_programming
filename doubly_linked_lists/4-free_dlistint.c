d7acacc9b8c30f98a1d715c3af0dde74da91947c
Meme-x03 <jmemelas.03@hotmail.com>
2024-07-12 19:57:28 +0000

    4-free_dlistint.c

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a dlistint_t list
+ * @head: head of the list
+ * Return: no return
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *node;
+
+	while (head)
+	{
+		node = head;
+		head = head->next;
+		free(node);
+	}
+}
