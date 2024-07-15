9db4be433260a3a7b67a6706a4413c0b78199e34
Meme-x03 <jmemelas.03@hotmail.com>
2024-07-15 03:30:50 +0000

    8-delete_dnodeint.c

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - Deletes a node from a dlistint_t 
+ * delete_dnodeint_at_index - Deletes a node from a dlistint_t
  * @head: A pointer to the head of the dlistint_t
  * @index: The index of the node to delete
  * Return: Upon Success - 1. Otherwise - -1.
